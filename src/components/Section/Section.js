21d101d908190eca279ee6280b09214f7baa49dc
VladyslavLata <vladlatahtc@gmail.com>
2022-08-12 13:18:55 +0300

    1

--- src/components/Section/Section.js
+++ src/components/Section/Section.js
@@ -1,7 +1,7 @@
 import { Box } from 'components/Box/Box';
 
 export const Section = ({ title, children }) => (
-  <Box p={4}>
+  <Box as="section" p={4}>
     <h2>{title}</h2>
     {children}
   </Box>
